3b49fd1d4e82a5381b30a6c61f100bf70a505d96
hossein-fallah <fallah.fcs@gmail.com>
2020-05-30 21:00:37 +0430

    added some comments

--- age.py
+++ age.py
@@ -1,20 +1,24 @@
+#here you give your age
 a=input('How old are you? (number please):  ')
 
+#calculater
 while(1):
     try:
-        age=float(a)
+        age=float(a)                                                       #     converts it to float
         age_in_days= age * 365
         age_in_months= age * 12
-        print('\nYou have lived for', round (age_in_months), 'months ')
+        print('\nYou have lived for', round (age_in_months), 'months ')    #     prints the results
 
-        print('and')
+        print('and')                                                       #     prints the results
 
-        print('You have lived for', round(age_in_days), 'days\n')
+        print('You have lived for', round(age_in_days), 'days\n')          #     prints the results
         break
         
-    except ValueError:
+    except ValueError:                                                     #     exception if you enter string
         print ("please give me a number")
         a=input('How old are you? (number please):  ')
+        
+        
         
     
 
