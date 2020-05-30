8c9017c0c9c9ca44287979d36cdc303deed00d07
hossein <fallah.fcs@gmail.com>
2020-05-30 20:05:30 +0430

    converting string to float is fixed

--- age.py
+++ age.py
@@ -1,27 +1,20 @@
-a=float(input('How old are you? (number please):  '))
-                
-age_in_days= a * 365
-age_in_months= a * 12
+a=input('How old are you? (number please):  ')
 
+while(1):
+    try:
+        age=float(a)
+        age_in_days= age * 365
+        age_in_months= age * 12
+        print('\nYou have lived for', round (age_in_months), 'months ')
 
+        print('and')
 
-while a <= 0 :
-    print('please enter a number grater than 0 ')
-    a=float(input('How old are you? (number please):  '))
-
-
-else:
-    print('You have lived for', age_in_months, 'months ')
-
-    print('and')
-
-    print('You have lived for', age_in_days, 'days')
-
-
-
-
-
-
-
+        print('You have lived for', round(age_in_days), 'days\n')
+        break
+        
+    except ValueError:
+        print ("please give me a number")
+        a=input('How old are you? (number please):  ')
+    
 
 
